f0cc920828dfc8260cff915a530db0d4344b4ca4
espana01-code <cespana@unipacifico.edu.co>
2025-08-25 20:14:40 -0500

    Primer commit

---
+++ taller.java
@@ -0,0 +1,15 @@
+import java.uutil.Scanner;
+public class taller {
+public static void main(String[] args) {
+    scanner in new Scanner(System.in);
+    int num1, num2, suma;
+    Scanner .System.out.println("Ingrese un numero:  ");
+    int1 = in.nexInt();
+    Scanner .System.out.println("Ingrese otro numero:  ");
+    int2 = in.nexInt();
+    suma = num1 + num2;
+    
+    System.out.println("La suma es " + suma);
+
+    }
+}
