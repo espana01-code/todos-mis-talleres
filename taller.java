d46f5ddb67c3712b0181f92bfd59e3866fb33d07
espana01-code <cespana@unipacifico.edu.co>
2025-08-25 20:32:30 -0500

    actualizacion

--- taller.java
+++ taller.java
@@ -1,19 +1,20 @@
-import java.uutil.Scanner;
+import java.util.Scanner;
+
 public class taller {
-public static void main(String[] args) {
-    scanner in new Scanner(System.in);
-    int num1, num2, suma;
-    Scanner .System.out.println("Ingrese un numero:  ");
-    int1 = in.nexInt();
-    Scanner .System.out.println("Ingrese otro numero:  ");
-    int2 = in.nexInt();
-    suma = num1 + num2;
-    
-    System.out.println("La suma es " + suma);
-    if (suma > 10){
-        System.out.println("La suma es mayor que 10" + suma);
-    }else{
-        System.out.println("La suma es menor que 10" + suma);
-    }
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int num1, num2, suma;
+        System.out.println("Ingrese un numero:  ");
+        num1 = in.nextInt();
+        System.out.println("Ingrese otro numero:  ");
+        num2 = in.nextInt();
+        suma = num1 + num2;
+
+        System.out.println("La suma es " + suma);
+        if (suma > 10) {
+            System.out.println("La suma es mayor que 10: " + suma);
+        } else {
+            System.out.println("La suma es menor o igual que 10: " + suma);
+        }
     }
 }
