96a0b19304fd409c2c87994e674cb9f9aadd7dd6
Carlos España <cespana@unipacifico.edu.co>
2025-08-30 09:44:15 -0500

    Actualización

--- taller.java
+++ taller.java
@@ -4,17 +4,27 @@ public class taller {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int num1, num2, suma;
-        System.out.println("Ingrese un numero:  ");
+        
+        // Ingreso de números
+        System.out.println("Ingrese un número:  ");
         num1 = in.nextInt();
-        System.out.println("Ingrese otro numero:  ");
+        System.out.println("Ingrese otro número:  ");
         num2 = in.nextInt();
+        
+        // Sumar los números
         suma = num1 + num2;
-
         System.out.println("La suma es " + suma);
+        
+        // Condición para verificar si la suma es mayor que 10
         if (suma > 10) {
             System.out.println("La suma es mayor que 10: " + suma);
         } else {
             System.out.println("La suma es menor o igual que 10: " + suma);
         }
+
+        
+        
+        // Cerrar el objeto Scanner
+        in.close();
     }
 }
